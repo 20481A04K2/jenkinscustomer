3c0730bd54eddee59dc95dc532274a2e4c2ac5cd
SAJJA VAMSI <99644384+20481A04K2@users.noreply.github.com>
2025-11-05 12:14:50 +0530

    Update deployApp.groovy

--- phyllo-shared-lib/vars/deployApp.groovy
+++ phyllo-shared-lib/vars/deployApp.groovy
@@ -1,1 +1,11 @@
 
+// shared-library/vars/deployApp.groovy
+def call(String bucketName) {
+    echo "🚀 Deploying app to GCP bucket: ${bucketName}"
+    sh """
+        echo 'Deleting old files from GCS...'
+        gsutil -m rm -r gs://${bucketName}/** || true
+        echo 'Uploading new build files...'
+        gsutil -m cp -r build/* gs://${bucketName}/
+    """
+}
