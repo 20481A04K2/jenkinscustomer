bc5dddec5daaa7bc17b2f2d9fb80ff606c0b1a8d
SAJJA VAMSI <99644384+20481A04K2@users.noreply.github.com>
2025-11-05 11:59:30 +0530

    Refactor deployApp function to use bucket name

--- shared-library/vars/deployApp.groovy
+++ shared-library/vars/deployApp.groovy
@@ -1,8 +1,10 @@
-def call(String appName, String env) {
-    echo "🚀 Deploying ${appName} to ${env}"
+// shared-library/vars/deployApp.groovy
+def call(String bucketName) {
+    echo "🚀 Deploying app to GCP bucket: ${bucketName}"
     sh """
-        echo "Deploying ${appName}..."
-        sleep 2
-        echo "✅ Successfully deployed ${appName} to ${env}"
+        echo 'Deleting old files from GCS...'
+        gsutil -m rm -r gs://${bucketName}/** || true
+        echo 'Uploading new build files...'
+        gsutil -m cp -r build/* gs://${bucketName}/
     """
 }
